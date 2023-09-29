92a80b38e22239a1630e6f51ac3e515206f3d6b2
Taylor Aukward <tayloraukward@Taylors-MacBook-Air.local>
2023-09-29 14:01:52 -0700

    added new TodoForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import "./styles.css"
 import {useState} from "react";
+import {TodoForm} from "./TodoForm.jsx";
 
 export default function App() {
-    const [newItem, setNewItem] = useState("")
     const [todos, setTodos] = useState([])
 
-    function handleSubmit(e) {
-        e.preventDefault()
-
+    function addTodo(title) {
         setTodos(currentTodos => {
             return [
                 ...currentTodos,
-                { id: crypto.randomUUID(), title: newItem, completed: false }
+                { id: crypto.randomUUID(), title: title, completed: false }
             ]
         })
         setNewItem("")
@@ -36,23 +34,16 @@ export default function App() {
 
     return (
     <>
-        <form onSubmit={handleSubmit} className="new-item-form">
-            <div className="form-row">
-                <label htmlFor="item">New Item</label>
-                <input
-                    value={newItem}
-                    onChange={e => setNewItem(e.target.value)}
-                    type="text"
-                    id="item" />
-            </div>
-            <button className="btn">Add</button>
-        </form>
+        <TodoForm onSubmit={addTodo}/>
         <h1 className="header">Todo List</h1>
         <ul className="list">
+            {todos.length === 0 && "No Todos"}
             {todos.map(todo => {
                 return <li key={todo.id}>
                     <label>
-                        <input onChange={e => toggleTodo(todo.id, e.target.checked)} type="checkbox" checked={todo.complete}/>
+                        <input onChange={e => toggleTodo(todo.id, e.target.checked)}
+                               type="checkbox"
+                               checked={todo.complete}/>
                         {todo.title}
                     </label>
                     <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
